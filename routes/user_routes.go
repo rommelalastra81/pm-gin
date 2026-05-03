73343acb78db1285bb177b54662f779188b97f30
rommelalastra81 <rommel.alastra@gmail.com>
2026-05-03 20:07:25 +0800

    from register user to create user

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -9,10 +9,10 @@ import (
 
 // RegisterAuthRoutes sets up all authentication-related routes.
 func UserRoutes(router *gin.Engine) {
-	auth := router.Group("/api/user")
+	auth := router.Group("/api/User")
 	{
 		// Public routes
-		auth.POST("/registeruser", controllers.RegisterUser)
+		auth.POST("/createuser", controllers.RegisterUser)
 
 		// Protected routes
 		auth.GET("/userprofile", middleware.AuthMiddleware(), controllers.GetUserProfile)
